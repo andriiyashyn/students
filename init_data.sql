bedca6d61440fff43886f6e14ce8bb8266a3cd07
Andrii Yashyn <andrii_yashyn@epam.com>
2018-05-30 09:11:14 +0300

    message

---
+++ init_data.sql
@@ -0,0 +1,19 @@
+INSERT INTO core_partytype(name, caption) VALUES 
+  ('PERSON', 'PERSON'), 
+  ('ORGANIZATION', 'ORGANIZATION');
+
+INSERT INTO core_organizationcategory(caption) VALUES 
+  ('Кафедра'),
+  ('Група');
+
+INSERT INTO core_personcategory(caption) VALUES 
+  ('Співробітник'),
+  ('Студент');
+
+INSERT INTO core_contacttype(name, template) VALUES
+  ('Мобільний телефон', '+380653432989'),
+  ('Додатковий телефон', '+380653432989'),
+  ('Адреса', 'вул. Академіка Янгеля, 5'),
+  ('Мобільний телефон матері', '+380653432989'),
+  ('Мобільний телефон батька', '+380653432989'),
+  ('Кімната в гуртожитку', '123');
